103684c52d6d302829299e9103370d7ffc9f09c3
tarasowski <dimitri+github@tarasowski.de>
2019-02-13 07:51:08 +0100

    local storage added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
 import {getInputValueFromId} from '../utils/ref.js'
 export const ADD_QUESTION = 'ADD_QUESTION'
+export const SAVE_TO_LOCAL_STORAGE = 'SAVE_TO_LOCAL_STORAGE'
 
-export const addQuestion = (getValue = getInputValueFromId) =>
+export const addQuestion = ({
+  question = getInputValueFromId('question'), 
+  askee = getInputValueFromId('askee')} = {}) =>
 ({
   type: ADD_QUESTION,
-  question: getValue('question'),
-  askee: getValue('askee')
+  question,
+  askee,
+  command: {
+    type: SAVE_TO_LOCAL_STORAGE,
+    question,
+    askee
+  } 
 })
--- src/side-effects/index.js
+++ src/side-effects/index.js
@@ -1,8 +1,11 @@
-export const SAVE_TO_LOCAL_STORAGE = 'SAVE_TO_LOCAL_STORAGE'
+const {SAVE_TO_LOCAL_STORAGE} = require('../actions/index.js')
 
 export const perform = dispatch => state => ({command}) => {
   switch(command.type) {
     case SAVE_TO_LOCAL_STORAGE:
+    document.getElementById('question').value = ''
+    document.getElementById('askee').value = ''
+    localStorage.setItem('question',JSON.stringify(state))
       break
     default:
       return state
